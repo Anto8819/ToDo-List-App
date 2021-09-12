7bbaed24796be6bc9318ec648fd7b2234523f663
Antonella Rapuano <mrap1988@gmail.com>
2021-09-12 01:26:41 -0300

    Updates

---
+++ precache-manifest.acff1ea18698e6a8c1d8c033bf273205.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb5d9b397a1f4486da853be36b740a7",
+    "url": "/ToDo-List-App/index.html"
+  },
+  {
+    "revision": "ccb35ee91463384ff0a6",
+    "url": "/ToDo-List-App/static/css/main.b56bf8f1.chunk.css"
+  },
+  {
+    "revision": "d4ab304391ec5bad2e3d",
+    "url": "/ToDo-List-App/static/js/2.37ac87a6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ToDo-List-App/static/js/2.37ac87a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccb35ee91463384ff0a6",
+    "url": "/ToDo-List-App/static/js/main.1e90be2e.chunk.js"
+  },
+  {
+    "revision": "af87380636fa545f2727",
+    "url": "/ToDo-List-App/static/js/runtime-main.832ebdf8.js"
+  },
+  {
+    "revision": "c4870551235a09a631512221c44e4d42",
+    "url": "/ToDo-List-App/static/media/check.c4870551.png"
+  }
+]);
